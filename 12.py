89064ae3a3aa176866031216ae876ace120b0fc3
Xabier Larrakoetxea Gallego <slok69@gmail.com>
2011-07-07 19:27:02 +0200

    Added level 12 script

--- 12.py
+++ 12.py
@@ -1,29 +1,32 @@
 
-import Image
-import ImageFilter
+#we have: various images (gif, jpg...) and one is gfx, in the gfx file there are 5 images
+#           this images are shuffeled, so we have to cut in 5 pieces that .gfx and make 5 images
+#           (one hint maybe for the next level is 'bert is evil' chan chan! )
+#Solution: disproportional
 
-#imgPath = '/home/slok/python-challenge/data/evil1.jpg'
-imgPath = '/home/slok/python-challenge/data/evil2.gfx'
+#unshuffle the 5 images from evil2.gfx
+trollImagePath = '/home/slok/python-challenge/data/evil2.gfx'
+trollImg = open(trollImagePath, 'r')
+trollImg = trollImg.read()
+cont = 0
+imgList =  ['','','','','']
+cont2 = 0
+for i in trollImg:
+    if cont > 4:
+        cont = 0
+    imgList[cont] += i
+    cont += 1
 
-size = (640, 480)
-im2 = Image.new("RGB", size, 0)
-pixelsOut = im2.load()
+#save the 5 images
+imgPath = '/home/slok/python-challenge/data/'
+imgNames = ['img1', 'img2', 'img3', 'img4', 'img5']
+imgExt= ['jpg', 'png', 'gif', 'png', 'jpg']
+cont = 0
 
-#load image
-im = Image.open(imgPath)
-pixels = im.load()
-
-for x in range(0,640):
-    for y in range(0,480):
-        pix = pixels[x, y]
-        if pix[0] != 0:
-            pixelsOut[x, y] = (pix[0],pix[1],pix[2])
-        
-#imFilter = im.filter(ImageFilter.MinFilter(1))
-#imFilter.save('/home/slok/python-challenge/data/evil1out.jpg', "JPEG") 
+for i in imgList:
+    f = open(imgPath + imgNames[cont] +'.'+ imgExt[cont], 'wb')
+    f.write(i)
+    f.close()
+    cont += 1
 
 
-im2.save('/home/slok/python-challenge/data/evil1out.jpg', "JPEG") 
-
-print im.info
-#print im.info['exif']
